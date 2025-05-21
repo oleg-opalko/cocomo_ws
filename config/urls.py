6922dab4deaa51c4f344a6f056c8c2832af3fdeb
Oleh Opalko <oopalko96@gmail.com>
2025-05-21 21:43:24 +0300

    added main app to project

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='idex')
+    
 ]
